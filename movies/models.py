81c109a235826fe0619a68332ed05fc6e124cb61
Ruslan9111 <ruslan_ru911@mail.ru>
2022-12-04 23:37:24 +0500

    initial commit

---
+++ movies/models.py
@@ -0,0 +1,121 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
+from ckeditor.fields import RichTextField
+
+
+
+class Movies(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название фильма")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    plot = models.TextField(blank=True, verbose_name="Сюжет")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('movie', kwargs={'movie_slug': self.slug})
+
+    class Meta:
+        verbose_name = "Фильмы"
+        verbose_name_plural = "Фильмы"
+        ordering = ['?']
+
+
+class Staff(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Имя фамилия")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    bio = models.TextField(blank=True, verbose_name="Сюжет")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    movies = models.ManyToManyField('Movies')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('staff', kwargs={'post_slug': self.slug})
+
+    class Meta:
+        verbose_name = "Актеры и режиссеры"
+        verbose_name_plural = "Актеры и режиссеры"
+        ordering = ['?']
+
+
+class Category(models.Model):
+    title_cat = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.title_cat
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name = "Категории"
+        verbose_name_plural = "Категории"
+        ordering = ['?']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=2055, null=True, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    pinterest_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='автор')
+    body = RichTextField(blank=True, null=True)
+    post_date = models.DateField(auto_now_add=True, verbose_name='дата публикации')
+    likes = models.ManyToManyField(User, related_name='movie_review', verbose_name='Нравится')
+
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('home')
+        # return reverse('article-detail', args=(str(self.id)))
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Movies, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
+
+
+
+
+
+
+
+
+
