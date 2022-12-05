950df5fecf5feb35d456bdc315e51eddc3a10a7d
Ruslan9111 <ruslan_ru911@mail.ru>
2022-12-05 19:42:06 +0500

    view_by_categories

--- movies/urls.py
+++ movies/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, re_path
 from django.views.decorators.cache import cache_page
 
-from .views import MoviesHome, CategoriesList
+from .views import MoviesHome, CategoriesList, MoviesByCategories
 
 
 urlpatterns = [
-    path('', MoviesHome.as_view()),
-    path('categories', CategoriesList.as_view())
-
-
-
+    path('', MoviesHome.as_view(), name='home'),
+    path('categories', CategoriesList.as_view(), name='list_of_categories'),
+    path('category/<slug:cat_slug>/', MoviesByCategories.as_view(), name='movies_by_category'),
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView
 from .models import Category, Movies
 
 
+# отображение домашней страницы
 class MoviesHome(ListView):
     model = Movies
     template_name = "movies/home.html"
@@ -11,9 +12,14 @@ class MoviesHome(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['movies_list'] = Movies.objects.all().order_by('title')
+        context['cat_selected'] = 0
         return context
 
+    def get_queryset(self):
+        return Movies.objects.filter(is_published=True)
 
+
+# отображение списка категорий
 class CategoriesList(ListView):
     model = Category
     template_name = "movies/list_of_categories.html"
@@ -25,9 +31,19 @@ class CategoriesList(ListView):
         return context
 
 
-class MovieDetailView(DetailView):
+# отображение фильмов определнной категории
+class MoviesByCategories(ListView):
     model = Movies
+    template_name = "movies/movies_by_category.html"
+    context_object_name = 'movies_by_category'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_queryset(self):
+        return Movies.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True )
+
+
+# class MovieDetailView(DetailView):
+#     model = Movies
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
 
